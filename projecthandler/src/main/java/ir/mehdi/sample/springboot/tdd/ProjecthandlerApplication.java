448e52c131f27bb9187553debb6f217ec15af1eb
Mahdi Shivaeifar <shivaeifar.m@gmail.com>
2025-02-11 15:13:46 +0330

    init commit

---
+++ projecthandler/src/main/java/ir/mehdi/sample/springboot/tdd/ProjecthandlerApplication.java
@@ -0,0 +1,13 @@
+package ir.mehdi.sample.springboot.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecthandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecthandlerApplication.class, args);
+	}
+
+}
---
+++ projecthandler/src/test/java/ir/mehdi/sample/springboot/tdd/ProjecthandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mehdi.sample.springboot.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecthandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
